9491a0b0e8d6a3a7672cc979f20a1f4e854de927
FarmProgrammer <104975790+FarmProgrammer@users.noreply.github.com>
2024-10-29 20:40:12 +0000

    Pursuit

--- Assets/Bot.cs
+++ Assets/Bot.cs
@@ -24,9 +24,27 @@ public class Bot : MonoBehaviour
         agent.SetDestination(transform.position - fleeVector);
     }
 
+    void Pursue(Vector3 location)
+    {
+        Vector3 targetDir = location - this.transform.position;
+
+        float relativeHeading = Vector3.Angle(transform.forward, transform.TransformVector(target.transform.forward));
+        float toTarget = Vector3.Angle(transform.forward, transform.TransformVector(targetDir));
+
+        if (toTarget<90 && relativeHeading<20 || target.GetComponent<Drive>().currentSpeed < 0.01f)
+        {
+            Seek(location);
+            return;
+        }
+
+        float lookAhead = targetDir.magnitude / (agent.speed + target.GetComponent<Drive>().currentSpeed);
+
+        Seek(location + target.transform.forward * lookAhead);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Flee(target.transform.position);
+        Pursue(target.transform.position);
     }
 }
