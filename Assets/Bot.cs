e599aa10393e0065204f4937f85af2e204f7f0df
FarmProgrammer <104975790+FarmProgrammer@users.noreply.github.com>
2024-10-29 20:53:26 +0000

    Evade

--- Assets/Bot.cs
+++ Assets/Bot.cs
@@ -7,10 +7,12 @@ public class Bot : MonoBehaviour
 {
     NavMeshAgent agent;
     public GameObject target;
+    Drive targetMotion;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        targetMotion = target.GetComponent<Drive>();
     }
 
     void Seek(Vector3 location)
@@ -31,20 +33,38 @@ public class Bot : MonoBehaviour
         float relativeHeading = Vector3.Angle(transform.forward, transform.TransformVector(target.transform.forward));
         float toTarget = Vector3.Angle(transform.forward, transform.TransformVector(targetDir));
 
-        if (toTarget<90 && relativeHeading<20 || target.GetComponent<Drive>().currentSpeed < 0.01f)
+        if (toTarget<90 && relativeHeading<20 || targetMotion.currentSpeed < 0.01f)
         {
             Seek(location);
             return;
         }
 
-        float lookAhead = targetDir.magnitude / (agent.speed + target.GetComponent<Drive>().currentSpeed);
+        float lookAhead = targetDir.magnitude / (agent.speed + targetMotion.currentSpeed);
 
         Seek(location + target.transform.forward * lookAhead);
+    }
+
+    void Evade(Vector3 location)
+    {
+        Vector3 targetDir = location - this.transform.position;
+
+        float relativeHeading = Vector3.Angle(transform.forward, transform.TransformVector(target.transform.forward));
+        float toTarget = Vector3.Angle(transform.forward, transform.TransformVector(targetDir));
+
+        if (toTarget < 90 && relativeHeading < 20 ||targetMotion.currentSpeed < 0.01f)
+        {
+            Flee(location);
+            return;
+        }
+
+        float lookAhead = targetDir.magnitude / (agent.speed + targetMotion.currentSpeed);
+
+        Flee(location + target.transform.forward * lookAhead);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Pursue(target.transform.position);
+        Evade(target.transform.position);
     }
 }
