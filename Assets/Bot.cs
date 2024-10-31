015ee6a81dcf81992d834657fa5822207fbc9171
FarmProgrammer <104975790+FarmProgrammer@users.noreply.github.com>
2024-10-31 17:21:04 +0000

    Hide Part 2

--- Assets/Bot.cs
+++ Assets/Bot.cs
@@ -78,6 +78,37 @@ public class Bot : MonoBehaviour
         Seek(targetWorld);
     }
 
+    void CleverHide()
+    {
+        float dist = Mathf.Infinity;
+        Vector3 chosenSpot = Vector3.zero;
+        Vector3 chosenDir = Vector3.zero;
+        GameObject chosenGO = World.Instance.GetHidingSpots()[0];
+
+        for (int i = 0; i < World.Instance.GetHidingSpots().Length; i++)
+        {
+            Vector3 hideDir = World.Instance.GetHidingSpots()[i].transform.position - target.transform.position;
+            Vector3 hidePos = World.Instance.GetHidingSpots()[i].transform.position + hideDir.normalized * 5;
+
+            if(Vector3.Distance(transform.position, hidePos) < dist)
+            {
+                chosenSpot = hidePos;
+                chosenDir = hideDir;
+                chosenGO = World.Instance.GetHidingSpots()[i];
+
+                dist = Vector3.Distance(this.transform.position, hidePos);
+            }
+        }
+
+        Collider hideCol = chosenGO.GetComponent<Collider>();
+        Ray backRay = new Ray(chosenSpot, -chosenDir.normalized);
+        RaycastHit info;
+        float distance = 100;
+        hideCol.Raycast(backRay, out info, distance);
+
+        Seek(info.point + chosenDir.normalized * 5);
+    }
+
     void Hide()
     {
         float dist = Mathf.Infinity;
@@ -88,7 +119,7 @@ public class Bot : MonoBehaviour
             Vector3 hideDir = World.Instance.GetHidingSpots()[i].transform.position - target.transform.position;
             Vector3 hidePos = World.Instance.GetHidingSpots()[i].transform.position + hideDir.normalized * 5;
 
-            if(Vector3.Distance(transform.position, hidePos) < dist)
+            if (Vector3.Distance(transform.position, hidePos) < dist)
             {
                 chosenSpot = hidePos;
                 dist = Vector3.Distance(this.transform.position, hidePos);
